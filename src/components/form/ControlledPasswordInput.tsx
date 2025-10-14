2d02a179e1b3b8a7eb5f9570326e4fba679919cf
Gokul-Gopi <gokulg1412@gmail.com>
2025-10-14 17:00:28 +0000

    feat: button to hide/show password input

--- src/components/form/ControlledPasswordInput.tsx
+++ src/components/form/ControlledPasswordInput.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../ui/Input";
 import { useController } from "react-hook-form";
+import { Icon } from "@iconify/react";
 
 interface IControlledPasswordInputProps extends React.ComponentProps<"input"> {
   name: string;
@@ -10,17 +11,25 @@ const ControlledPasswordInput = ({
   name,
   ...props
 }: IControlledPasswordInputProps) => {
+  const [hidePassword, setHidePassword] = useState(true);
+
   const {
     field,
     fieldState: { error },
   } = useController({ name });
 
   return (
-    <div className="w-full">
-      <Input type="password" {...field} {...props} />
+    <div className="relative w-full">
+      <Input type={hidePassword ? "password" : "text"} {...field} {...props} />
       {error && (
         <p className="text-destructive mt-1.5 text-xs">{error.message}</p>
       )}
+
+      <Icon
+        onClick={() => setHidePassword((pre) => !pre)}
+        icon={hidePassword ? "tabler:eye-off" : "tabler:eye"}
+        className="absolute top-2.5 right-3"
+      />
     </div>
   );
 };
